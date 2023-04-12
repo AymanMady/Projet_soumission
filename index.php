6989419395ba5171d1647d14d68ab8fefa24d3e9
Ayman <22014@supnum.mr>
2023-04-12 01:29:39 +0000

    creation de la page admin

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <?php
 session_start();
 
-// Vérifier si la session est active
 if (!$_SESSION['admin']) {
   header('Location: authentification.php');
   session_destroy();
   exit();
 }
-// Désactiver la mise en cache de la page précédente
-header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
-header('Pragma: no-cache');
-header('Expires: Thu, 01 Jan 1970 00:00:00 GMT');
 
 ?>
 <!DOCTYPE html>
@@ -35,7 +30,8 @@ header('Expires: Thu, 01 Jan 1970 00:00:00 GMT');
     </ul>
   </div>
   <div class="content">
-    <!-- Contenu de la page -->
+
+  
   </div>
 </body>
 
