8b5383feb8ec627248933b630e61a0c78763aafc
souleyman meden <22018@supnum.mr>
2023-05-09 00:24:49 +0000

    rrrr

--- admin/supprimer_utilisateur.php
+++ admin/supprimer_utilisateur.php
@@ -2,5 +2,5 @@
   include_once "../connexion.php";
   $id_user= $_GET['id_user'];
   $req = mysqli_query($conn , "DELETE FROM utilisateur WHERE id_user = $id_user");
-  header("Location:utilisateur.php")
+  header("Location:utilisateurs.php")
 ?>
